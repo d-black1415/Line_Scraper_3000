adaffa6bfa2ff1a29355a8c373d731b9b5d1cda3
David Iola <iola2@illinois.edu>
2020-11-19 22:32:17 -0800

    pgw bug fix for teams with missing lines

--- src/util/helpers.py
+++ src/util/helpers.py
@@ -26,11 +26,28 @@ def retrieve_data_frame_for_game(game):
         app_list[1] = date_td.text.split(' ')[1]
     app_list[2] = td_arr[TEAM_ID_IDX].text.strip()
     app_list[4] = td_arr[TEAM_IDX].text.strip()
-    spread_input_value = td_arr[SPREAD_IDX].input['value']
-    app_list[3] = spread_input_value.split('_')[1]
-    app_list[5] = spread_input_value.split('_')[2]
-    app_list[6] = spread_input_value.split('_')[3]
-    total_input_value = td_arr[TOTAL_IDX].input['value']
-    app_list[7] = total_input_value.split('_')[2]
-    app_list[8] = total_input_value.split('_')[3]
+
+    spread_td_elem = td_arr[SPREAD_IDX]
+    if spread_td_elem.input is not None:
+        spread_input_value = spread_td_elem.input['value']
+        app_list[3] = spread_input_value.split('_')[1]
+        app_list[5] = spread_input_value.split('_')[2]
+        app_list[6] = spread_input_value.split('_')[3]
+    else:
+        app_list[3] = "Not Found"
+        app_list[5] = "Not Found"
+        app_list[6] = "Not Found"
+        print("Team: {} lacks spread value".format(app_list[4]))
+
+    total_td_elem = td_arr[TOTAL_IDX]
+    if total_td_elem.input is not None:
+        total_input_value = total_td_elem.input['value']
+        app_list[7] = total_input_value.split('_')[2]
+        app_list[8] = total_input_value.split('_')[3]
+    else:
+        app_list[7] = "Not Found"
+        app_list[8] = "Not Found"
+        print("Team: {} lacks total value".format(app_list[4]))
+
+
     return app_list
